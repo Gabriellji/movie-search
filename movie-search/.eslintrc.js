8a1d2408670a6fd4c0da38c91755526e429bc500
Evgeniya Gabrikova <gabriellji@yahoo.com>
2020-04-27 22:12:13 +0300

    feat: add basic app layout

---
+++ movie-search/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "linebreak-style": 0,
+    "indent": ["error", "tab"],
+    "no-tabs": 0,
+    "no-underscore-dangle": 'off'
+
+  }
+};
